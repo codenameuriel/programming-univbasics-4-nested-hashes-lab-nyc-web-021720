12822dc8dce1d49f750d6f8bac255e36882f5948
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-18 23:04:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,9 +65,10 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-  :yukihiro_matsumoto => {
+  {:yukihiro_matsumoto => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
+    }
   }
   
   programmer_hash = :yukihiro_matsumoto.merge(programmer_hash)
